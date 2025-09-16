12d3198ed8490a50cbdc6ecf3a07610b275664fd
Pedro Gonçalves <Pedro-g2>
2025-09-16 18:16:42 -0300

    add id na resposta json

--- app/Http/Resources/ProfessorResource.php
+++ app/Http/Resources/ProfessorResource.php
@@ -15,6 +15,7 @@ class ProfessorResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'nome' => $this->nome,
             'data_nascimento' => $this->data_nascimento,
             'cpf' => $this->cpf,
